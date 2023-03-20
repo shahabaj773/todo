6fa723acf886d12e17d8b5e7fa3384e0f0b6123b
shahabaj773 <shahabaj773@gmail.com>
2023-03-20 17:17:49 +0530

    hugh

--- backend/Server.js
+++ backend/Server.js
@@ -6,14 +6,14 @@ const PORT = 4000;
 const mongoose = require('mongoose');
 var jwt = require('jsonwebtoken');
 const cors = require('cors');
+var cookieParser = require('cookie-parser');
 require('dotenv').config();
 const nodemailer = require('nodemailer');
-
 //self signed error - resolver
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 mongoose.set('strictQuery', false);
-
+app.use(cookieParser());
 const corsOptions = {
   origin: 'http://localhost:3000',
   credentials: true,
@@ -61,9 +61,28 @@ app.post('/signup', async (req, res) => {
   // var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
 });
 app.post('/login', async (req, res) => {
-  var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
-  const founduser = User.findOne({ name: req.body.name });
+  const founduser = await User.findOne({ email: req.body.email });
   if (founduser) {
+    const match = await bcrypt.compare(req.body.password, founduser.password);
+    if (match) {
+      // const token = jwt.sign(
+      //   { username: founduser.name },
+      //   process.env.JWT_KEY,
+      //   {
+      //     expiresIn: '15d',
+      //   }
+      // );
+      const options = {
+        httpOnly: true,
+        expires: new Date(Date.now() + 36000000),
+      };
+      res.cookie('token', founduser.token, options);
+      res.send(founduser);
+    } else {
+      console.log('invalid password');
+    }
+  } else {
+    console.log('no user');
   }
 });
 
@@ -94,6 +113,13 @@ const UserSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  token: {
+    type: String,
+  },
+  isAdmin: {
+    type: Boolean,
+  },
+  todo: [{ todo: { type: String }, completed: { type: String } }],
 });
 const User = mongoose.model('User', UserSchema);
 
@@ -102,7 +128,10 @@ app.get('/find/usersdata', async (req, res) => {
 });
 
 app.post('/otp', (req, res) => {
-  if (req.body.otp === otp) {
+  if (req.body.otp == otp) {
+    const token = jwt.sign({ username: req.body.name }, process.env.JWT_KEY, {
+      expiresIn: '15d',
+    });
     bcrypt.hash(req.body.password, 15, async function (err, hash) {
       await User.insertMany([
         {
@@ -110,11 +139,80 @@ app.post('/otp', (req, res) => {
           email: req.body.email,
           password: hash,
           mobile: req.body.mobile,
+          token: token,
+          isAdmin: false,
         },
       ]);
     });
+    const options = {
+      httpOnly: true,
+      expires: new Date(Date.now() + 36000000),
+    };
+    res.cookie('token', token, options);
     res.send('success');
   } else {
     res.send('invalidotp');
   }
 });
+app.post('/remove/user/byid', async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.body.id);
+    res.send('Deleted');
+  } catch {
+    console.log('error');
+  }
+});
+const authenticate = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    //verify token remaining & cookie
+    const user = await User.findOne({ token: token });
+    if (!user) {
+      res.send('error');
+    } else {
+      req.user = user;
+      next();
+    }
+  } catch {}
+};
+
+app.get('/authenticate', authenticate, async (req, res) => {
+  res.send(req.user);
+});
+app.post('/add/todo', async (req, res) => {
+  await User.findOneAndUpdate(
+    { _id: req.body.id },
+    {
+      $push: {
+        todo: {
+          todo: req.body.todo,
+          completed: 'false',
+        },
+      },
+    }
+  );
+  res.send('submitted');
+});
+app.post('/delete/todo', async (req, res) => {
+  await User.findOneAndUpdate(
+    { _id: req.body.id },
+    {
+      $pull: {
+        todo: req.body.todo,
+      },
+    }
+  );
+  res.send('deleted');
+});
+
+// app.post('/update/todo', async (req, res) => {
+//   await User.findOneAndUpdate(
+//     { _id: req.body.id },
+//     {
+//       $pull: {
+//         todo: req.body.todo,
+//       },
+//     }
+//   );
+//   res.send('updated');
+// });
